ed58967f5075a2f86963fc29294453c618e63a29
loveCanopy <1039431583@qq.com>
2016-07-26 15:56:02 +0800

    插入排序

---
+++ 排序/插入排序/二分插入排序.java
@@ -0,0 +1,25 @@
+package com.sort;
+二分法插入排序的思想和直接插入一样，只是找合适的插入位置的方式不同，这里是按二分法找到合适的位置，可以减少比较的次数。
+
+    private static void sort(int[] a) {
+        for (int i = 0; i < a.length; i++) {
+            int temp = a[i];
+            int left = 0;
+            int right = i-1;
+            int mid = 0;
+            while(left<=right){
+                mid = (left+right)/2;
+                if(temp<a[mid]){
+                    right = mid-1;
+                }else{
+                    left = mid+1;
+                }
+            }
+            for (int j = i-1; j >= left; j--) {
+                a[j+1] = a[j];
+            }
+            if(left != i){
+                a[left] = temp;
+            }
+        }
+    }
